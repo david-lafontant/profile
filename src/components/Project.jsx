4a8abe04fcda2b775901cea9ebdbe39da5e5143e
David Lafontant <lafontant.david@gmail.com>
2024-12-19 11:58:27 -0500

    Create Project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Project = props => {
+    return (
+        <div>Project</div>
+    )
+}
+
+Project.propTypes = {}
+
+export default Project
