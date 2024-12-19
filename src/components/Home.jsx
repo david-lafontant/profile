e8fc4719be40da74e2ae62bb6e59f69248226c3f
David Lafontant <lafontant.david@gmail.com>
2024-12-19 16:45:16 -0500

    Update home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,20 +12,35 @@ export const Home = () => {
     };
 
     return (
-        <div className="container-fluid" id="home">
-            <h1>Web developer</h1>
-
-            <ul>
-                <li>
-                    <button onClick={onButtonClick} className='btn'>RESUME</button>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/david-lafontant/" target='_blank' className='btn'>LINKEDIN</a>
-                </li>
-                <li>
-                    <a href="https://github.com/david-lafontant" target='_blank' className='btn'>GITHUB</a>
-                </li>
-            </ul>
+        <div className="px-4 py-5 my-5 text-center">
+            <h1 className="display-3 fw-bold text-body-emphasis">I&apos;m David</h1>
+            <div className="col-lg-6 mx-auto">
+                <p className="lead mb-4">
+                    I&apos;m a full-stack web developer dedicated to bringing your vision to life. I deliver solutions that meet your needs and empower your business to thrive in an ever-evolving digital landscape.<br />Let&apos;s connect to turn your projects into success stories—reach out today, and let&apos;s start the conversation!
+                </p>
+                <ul className='d-md-flex gap-3 justify-content-center align-items-sm-center'>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>API</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>FRONTEND</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>BACKEND</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>RUBY ON RAILS</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>MERN</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>DJANGO</span></li>
+                    <li className='list-unstyled'><span className='badge bg-secondary p-2'>PERN</span></li>
+                </ul>
+                <ul className='d-md-flex gap-3 justify-content-center align-items-sm-center'>
+                    <li className='list-unstyled'>
+                        <button onClick={onButtonClick} className='btn button-5'><i class="bi bi-file-earmark-person"></i>RESUME</button>
+                    </li>
+                    <li className='list-unstyled'>
+                        <a href="https://www.linkedin.com/in/david-lafontant/" target='_blank' className='btn button-5'><i class="bi bi-linkedin"></i>LINKEDIN</a>
+                    </li>
+                    <li className='list-unstyled'>
+                        <a href="https://github.com/david-lafontant" target='_blank' className='btn button-5'><i class="bi bi-github"></i>GITHUB</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
+
+
