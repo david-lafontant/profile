b427f2429befa4319aa3ce7a972bceea0f5ae1c4
David Lafontant <lafontant.david@gmail.com>
2024-12-20 09:54:15 -0500

    Create data file

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        id: 1,
+        title: "project 1",
+        descripttion: "lorem ipsum dolor",
+        github: "https://www.github.com",
+        linkedin: "https://www.linkedin.com",
+        stack: ['html', 'css', 'javascript'],
+        picture: 'https://drive.google.com/file/d/12p4ErTn-ZqU6m8OrDZkszPv3eIjtaa5A/view?usp=sharing'
+    },
+    {
+        id: 1,
+        title: "project 2",
+        descripttion: "lorem 2ipsum dolor",
+        github: "https://www.github.com",
+        linkedin: "https://www.linkedin.com",
+        stack: ['ruby', 'css', 'javascript'],
+        picture: 'https://drive.google.com/file/d/12p4ErTn-ZqU6m8OrDZkszPv3eIjtaa5A/view?usp=sharing'
+    },
+
+    {
+        id: 3,
+        title: "project 3",
+        descripttion: "lorem 3ipsum dolor",
+        github: "https://www.github.com",
+        linkedin: "https://www.linkedin.com",
+        stack: ['ruby on rails', 'css', 'javascript'],
+        picture: 'https://drive.google.com/file/d/12p4ErTn-ZqU6m8OrDZkszPv3eIjtaa5A/view?usp=sharing'
+    }
+]
+
+export default data;
