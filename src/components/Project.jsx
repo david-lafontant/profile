88baaee28962df54583bdbde50e4017c756d6fc5
david-lafontant <lafontant.david@gmail.com>
2024-12-20 15:44:47 -0500

    Create project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,26 @@ import PropTypes from 'prop-types'
 
 const Project = props => {
     return (
-        <div>Project</div>
+      <div className="col col-md-4 p-2">
+
+        <div className= "card">
+        <img src={props.item.picture} alt={props.title} />
+        <div className="card-content">
+          <h2>
+            {props.item.title}
+          </h2>
+          <p>
+           {props.item.description}
+          </p>
+          <a href={props.item.github} className="button" target='_blank'>
+            Find out more 
+            <span className="material-symbols-outlined">
+            <i class="bi bi-chevron-double-right"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      </div>
     )
 }
 
