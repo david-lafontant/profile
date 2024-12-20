c17407dd028bc1c0d30b120bd2d5a6f6c2dfa2b4
David Lafontant <lafontant.david@gmail.com>
2024-12-20 09:54:43 -0500

    Import data in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import data from '../utils/data'
+import { use } from 'react'
+
 
 export const Projects = () => {
+    const [projects, setProject] = useState(data)
+
+
     return (
         <div>Projects</div>
     )
